c260424a1bedb72541b53329d8bc0ef53fadbf00
raibosome <raimi.bkarim@gmail.com>
2020-03-01 09:45:27 +0800

    Update example

--- examples/example1.py
+++ examples/example1.py
@@ -1,12 +1,12 @@
 import numpy as np
-from trees_and_forests import DecisionTreeRegressor
+from trees_and_forests import DecisionTreeRegressor, GradientBoostingRegressor
 
 X = np.array([
     [1, 101, 103, 3, 5, 107, 109, 7, 8],
     [1, 101, 103, 3, 5, 107, 109, 7, 8],
     [0,   0,   0, 0, 0,   0,   0, 1, 1]]).T
-y = np.array([13, 30, 22, 31, 16, 19, 18, 27, 15])
+y = np.array([13, 67, 66, 14, 14, 64, 63, 16, 15])
 
-rgr = DecisionTreeRegressor()
+rgr = GradientBoostingRegressor(5)
 rgr.fit(X,y)
 rgr.predict(X)
